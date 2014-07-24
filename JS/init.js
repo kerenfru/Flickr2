1e617e72905feb69d265a9f058a26b8675dd7102
hadarhod <kerenfruc@gmail.com>
2014-07-24 08:52:41 +0300

    Appending airbnb JS rules

--- JS/init.js
+++ JS/init.js
@@ -1,11 +1,12 @@
-var height = $(window).height();
-var width = $(window).width();
-var totalPhotos = 20;
-var flickerFeedUrl = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-var flickerFeedUrlAuthor = "http://api.flickr.com/services/feeds/photos_public.gne?id=AUTHORID&jsoncallback=?";
+
+var totalPhotos = 20,
+	width = $(window).width(),
+	height = $(window).height(),
+	flickerFeedUrl = 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?',
+	flickerFeedUrlAuthor = 'http://api.flickr.com/services/feeds/photos_public.gne?id=AUTHORID&jsoncallback=?';
 
 $(document).ready(function () {
-    console.log("ready!");
+    console.log('ready!');
 
     $('#loadFlickr').click(function () {
         loadFlickrAgain(flickerFeedUrl);
@@ -16,57 +17,52 @@ $(document).ready(function () {
 
 /* Get the images by the given Url */
 function getData(Url) {
-    var photosCounter = 0;
-    var rowX = (width / 7);
-    var rowY = 1;
-    var rowXcounter = 1;
+    var photosCounter = 0,
+		rowXcounter = 1,
+		lineheigth = 22,
+		photosinrow = 5,
+		rowX = (width / 7),
+		rowY = 1;
 
-    $("#loading").show();
+    $('#loading').show();
 
     $.getJSON(Url, {
-        format: "json"
+        format: 'json'
     })
       .done(function (data) {
           // open Box
-          $('.boxCover').addClass("openBoxCover");
+          $('.boxCover').addClass('openBoxCover');
 
           // wait for the box to be open
           setTimeout(function () {
 
               $.each(data.items, function (i, item) {
-                  var wrapper = $("<div class=\"gallery-item wrapper" + i + "\">").css("top", (height - 100) + "px").appendTo("#gallery");
+                  $('<div class=\"gallery-item wrapper' + i + '\">').css('top', (height - 100) + 'px').appendTo('#gallery');
 
                   // appending the photos to the gallery
                   setTimeout(function () {
+                      var posY = (rowY * lineheigth) + 'px',
+                          posX = (rowX * rowXcounter) + 'px',
+                          rotate = randomRotate();
 
-                      var rotateSign = Math.floor((Math.random() * 2) + 1);
-                      var rotateNum = Math.floor((Math.random() * 5) + 1);
-                      var posY = (rowY * 22) + "px";
-                      var posX = (rowX * rowXcounter) + "px";
-                      var rotate;
-
-                      if (((i + 1) % 5) === 0) {
+                      if (((i + 1) % photosinrow) === 0) {
                           rowY += 8;
                           rowXcounter = 0;
                       }
 
                       rowXcounter++;
-					  
-					  // generate random rotate degree
-                      if (rotateSign == 1) rotate = rotateNum;
-                      else rotate = "-" + rotateNum;
 
-                      $("<img class=\"photo\">").attr("src", item.media.m).appendTo(".wrapper" + i).load(
+                      $('<img class=\"photo\">').attr('src', item.media.m).appendTo('.wrapper' + i).load(
                           function () {
                               photosCounter++;
                               if (photosCounter === totalPhotos) {
-                                  $("#loading").hide();
-                                  $('.boxCover').removeClass("openBoxCover");
+                                  $('#loading').hide();
+                                  $('.boxCover').removeClass('openBoxCover');
                               }
                           }).parent().animate({
                               opacity: 1,
-                              width: "150px",
-                              height: "150px",
+                              width: '150px',
+                              height: '150px',
                               left: posX,
                               top: posY
                           },
@@ -84,27 +80,38 @@ function getData(Url) {
                           }, 'linear');
 
                       // add photo details
-                      $("<span class=\"photo-details hide\">").html(generateInfo(item)).appendTo(".wrapper" + i);
+                      $('<span class=\"photo-details hide\">').html(generateInfo(item)).appendTo('.wrapper' + i);
 
-                      $(".wrapper" + i).hover(function () {
-                          $(this).find('.photo-details').toggleClass("hide");
-                          $(this).find('img').toggleClass("fade");
+                      $('.wrapper' + i).hover(function () {
+                          $(this).find('.photo-details').toggleClass('hide');
+                          $(this).find('img').toggleClass('fade');
                       });
-
                   }, (i * 100));
               });
           }, 300);
       });
 }
 
+function randomRotate() {
+    var rotateSign = Math.floor((Math.random() * 2) + 1),
+		rotateNum = Math.floor((Math.random() * 9) + 1),
+		rotate;
+
+    // generate random rotate degree
+    if (rotateSign === 1) rotate = rotateNum;
+    else rotate = '-' + rotateNum;
+
+    return rotate;
+}
+
 /* Creates the photo information */
-function generateInfo(item){
-	var info = "<h5>Title: " + (item.title.length > 30 ? item.title.substring(0, 27) + "..." : item.title) + "</h5>" +
-               "<h6>Date: " + new Date(item.date_taken).toLocaleString() + "</h6>" +
-               "<h6>Author: <a href=\"#\" onclick=\"showAuthor('" + item.author_id + "')\">" + item.author.substring(item.author.indexOf("(") + 1, item.author.indexOf(")")) + "</a></h6>" +
-               "<h6>Link: <a href=\"" + item.media.m + "\">Click Here</a></h6>"
-			   
-	return info;
+function generateInfo(item) {
+    var info = '<h5>Title: ' + (item.title.length > 30 ? item.title.substring(0, 27) + '...' : item.title) + '</h5>' +
+               '<h6>Date: ' + new Date(item.date_taken).toLocaleString() + '</h6>' +
+               '<h6>Author: <a href=\"#\" onclick=\"showAuthor(&quot;' + item.author_id + '&quot;)\">' + item.author.substring(item.author.indexOf('(') + 1, item.author.indexOf(')')) + '</a></h6>' +
+               '<h6>Link: <a href=\"' + item.media.m + '\">Click Here</a></h6>';
+
+    return info;
 }
 
 /* Get photos by the given author */
@@ -114,7 +121,7 @@ function showAuthor(author) {
     setTimeout(function () {
         // Clear Gallery
         $('.gallery-item').remove();
-        getData(flickerFeedUrlAuthor.replace("AUTHORID", author));
+        getData(flickerFeedUrlAuthor.replace('AUTHORID', author));
     }, 3000);
 }
 
@@ -131,26 +138,19 @@ function loadFlickrAgain(Url) {
 
 /* Clear the gallery */
 function ClearGallery() {
-    var rotate = "-100";
+    var rotate;
 
     $.each([12, 3, 7, 18, 5, 0, 16, 6, 1, 17, 11, 2, 10, 4, 8, 13, 9, 14, 15, 19], function (index, value) {
+        rotate = randomRotate();
+        console.log(rotate);
         setTimeout(function () {
             $('.wrapper' + value).animate({
-                width: "150px",
-                height: "150px",
-                opacity: "0",
+                width: '150px',
+                height: '150px',
+                opacity: '0',
                 top: height - 180
             },
                 {
-                    step: function (now, fx) {
-                        $(this).css('padding', '5px 5px 5px 5px');
-                        $(this).css('background-color', 'white');
-                        $(this).css('-webkit-transform', 'rotate(' + rotate + 'deg)');
-                        $(this).css('-moz-transform', 'rotate(' + rotate + 'deg)');
-                        $(this).css('-ms-transform', 'rotate(' + rotate + 'deg)');
-                        $(this).css('-o-transform', 'rotate(' + rotate + 'deg)');
-                        $(this).css('transform', 'rotate(' + rotate + 'deg)');
-                    },
                     duration: 'slow'
                 }, 'linear');
         }, (index * 100));
